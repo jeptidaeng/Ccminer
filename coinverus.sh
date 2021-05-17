854f8736b67168122f66be339b6fa7b2d8162ec4
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2021-05-17 22:51:46 +0700

    Create coinverus.sh

---
+++ coinverus.sh
@@ -0,0 +1,8 @@
+# coinverus
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+ nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RS7Aor4qJQE9BgkVoGMtmS9MhT1EPeoZw7.VRCS -p x -t 4
+
+
