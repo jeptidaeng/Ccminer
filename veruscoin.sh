ef6718480be1049b206c816d1aa9135357c58088
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2021-05-18 00:04:30 +0700

    Update and rename coinverus.sh to veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,6 +1,6 @@
-
+#Veruscoin.sh
 sudo apt update
- ewget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+ewget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RS7Aor4qJQE9BgkVoGMtmS9MhT1EPeoZw7.verus -p x --cpu 4
 
