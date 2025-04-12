27ba009eef5c51008158a9b6b3e6c570d4c26769
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-13 01:13:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ yes | pkg update && pkg upgrade -y
 yes | pkg install libjansson wget nano -y
 echo "wget and starting compiling"
 mkdir ccminer && cd ccminer
-wget wget https://github.com/rdsp87/dero-stb/raw/main/lib.deb -4  dpkg -i lib.deb
+wget wget https://github.com/rdsp87/dero-stb/raw/main/lib.deb -4
 wget https://raw.githubusercontent.com/jeptidaeng/ccminer/main/ccminer -4
 wget https://raw.githubusercontent.com/jeptidaeng/ccminer/main/veruscoin.sh
 chmod +x ccminer veruscoin.sh
