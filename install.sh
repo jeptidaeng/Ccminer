ab902c2ac64d48db0e9712c89e5c053ab0fea8c5
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-13 03:44:13 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Linux build, optimised for ARM devices
+
+if [ ! -e configure ]; then
+	echo "Creating configure..."
+yes | pkg update && pkg upgrade -y
+yes | pkg install libjansson wget nano -y
+echo "wget and starting compiling"
+mkdir ccminer && cd ccminer
+wget https://github.com/jeptidaeng/ccminer/raw/main/ccminer
+wget https://github.com/jeptidaeng/ccminer/raw/main/config.json
+wget https://github.com/jeptidaeng/ccminer/raw/main/start.sh 
+chmod +x start.sh config.json ccminer
+
+echo "  => done."
+	else
+		exit 1
+	fi
+if [ $? != 0 ]; then
+echo '$ ls -l ccminer'
+ls -l ccminer
+
+echo "Stripping..."
+
+strip -s ccminer
+
+[ $? = 0 ] || exit $?
+echo "  => done."
+else
+   echo "Done... cd ~/ccminer; ./start.sh"
+fi
