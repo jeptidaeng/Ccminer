3b6d5b88498972288ad96bdd43355609a88f3bb4
Bangjay <52452968+jeptidaeng@users.noreply.github.com>
2025-04-13 03:32:11 +0700

    Binary file config.jon start,sh

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
+
+verus() {
+# Mining Algorithm
+ALGO=verus
+
+# Pool URL #
+POOL=stratum+tcp://sg.vipor.net:5040
+
+# Wallet address to mine (AVN coin recomended)#
+WALLET=RNbSbbtzt7jYeHHCaBeruw4TBtDqmwtor5
+
+# Pool Password (Optional) #
+PASS="trmx"
+
+# Mining thread to use #
+# To know how many your own cpu threads, #
+# Type lscpu on the terminal #
+THR=8
+
+# Miner config[Do not edit if you not understand] #
+
+clear
+./ccminer -a $ALGO -o $POOL -u $WALLET -p $PASS -t $THR
+}
+verus
